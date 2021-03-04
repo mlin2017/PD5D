7e166b491bac973456c1bef2e1863605f3f98501
jdparker101 <jparker4@sheffield.ac.uk>
2021-03-04 15:25:29 -0500

    Update cellranger_count_scRNAseq_submission.sh

--- scRNAseq/CellRanger_RNAseq/cellranger_count_scRNAseq_submission.sh
+++ scRNAseq/CellRanger_RNAseq/cellranger_count_scRNAseq_submission.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in $(cat annotations_021721.csv | awk -F "," '(NR>1){print $2}'); do
-    bsub -q big "sh cellranger_counts.sh $i"
+    bsub -q big -e {$i}_cellrangercounts.log "sh cellranger_counts.sh $i"
 done
