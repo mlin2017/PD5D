4a8057924be93b33163697709d38b3bfda4b6148
Parker <jap0606@login03.o2.rc.hms.harvard.edu>
2022-09-19 14:26:26 -0400

    second commit to delete duplicates

--- scRNA_and_scATAC-seq_Markdowns/batch1-22_HC_IntegrationAnalyses/scRNA-seq/snRNA-seq_Workflow_Part3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -t 96:00:00
-#SBATCH --mem=240G
-#SBATCH -p medium
-#SBATCH -e logs/Rscript-%J.log
-module load conda2/4.2.13
-source activate /home/jap0606/.conda/envs/condamamba/envs/r4-base
-Rscript snRNA-seq_Workflow_Part3.R
--- scRNA_and_scATAC-seq_Markdowns/batch1-8IntegrationAnalyses/scRNA-seq/Batch1to8_Integration_Workflow_Part4-2_Submission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -t 96:00:00
-#SBATCH --mem=200G
-#SBATCH -p medium
-#SBATCH -e logs/Rscript-%J.log
-for Ident in $(tail -n +2 Files/SubclusterIdents.txt); do
-    sbatch --export=clusterident=$Ident Batch1to8_Integration_Workflow_Part4-2.sh
-done
-
